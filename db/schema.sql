5a06c90579b3c09c0055e8fc4f62e9399ea9c397
Pita <salahbenalmadena@gmail.com>
2026-03-03 14:55:44 +0100

    SQL: Update > Add reviews, product category

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+-- USERS 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -8,6 +9,14 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- CATEGORIES
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- PRODUCTS 
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(200) NOT NULL,
@@ -15,11 +24,11 @@ CREATE TABLE products (
     price NUMERIC(10,2) NOT NULL,
     stock INTEGER NOT NULL DEFAULT 0,
     image_url TEXT,
+    category_id INTEGER REFERENCES categories(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
-
+-- ORDERS
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id),
@@ -29,6 +38,7 @@ CREATE TABLE orders (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- ORDER ITEMS
 CREATE TABLE order_items (
     id SERIAL PRIMARY KEY,
     order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
@@ -36,4 +46,15 @@ CREATE TABLE order_items (
     quantity INTEGER NOT NULL,
     price NUMERIC(10,2) NOT NULL,
     UNIQUE (order_id, product_id)
+);
+
+-- REVIEWS 
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(id),
+    rating INTEGER CHECK (rating >= 1 AND rating <= 5),
+    comment TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (product_id, user_id)
 );
